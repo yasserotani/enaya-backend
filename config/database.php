5dccb0f62b353ac678ba6eff7d02654cfc04b01b
yasserotani <yasserotane@gmail.com>
2026-04-20 14:59:32 +0300

    fix: disable SSL cert verification for Aiven

--- config/database.php
+++ config/database.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Str;
-use Pdo\Mysql;
+use PDO;
+
 
 return [
 
@@ -59,10 +60,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            'options' => extension_loaded('pdo_mysql') ? [
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ] : [],
         ],
 
         'mariadb' => [
