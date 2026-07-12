474b6a8c090b982e0e2c5edfad4dad2ada3d673d
yasserotani <yasserotane@gmail.com>
2026-07-12 21:00:31 +0300

    fix 5 add migrate route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/deploy/migrate', function (Illuminate\Http\Request $request) {
+    if ($request->header('X-Deploy-Secret') !== env('DEPLOY_SECRET')) {
+        abort(403);
+    }
+
+    Artisan::call('migrate', ['--force' => true]);
+
+    return response()->json(['output' => Artisan::output()]);
+});
