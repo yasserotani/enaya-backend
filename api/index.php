96789b2b6e5093e6be9cb246b004282b4b5ebb1a
yasserotani <yasserotane@gmail.com>
2026-07-13 00:15:28 +0300

    fix index.php 1

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// 1. Force the bootstrap process to load Laravel correctly inside Vercel's folder structure
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 2. Handle the incoming request through the HTTP Kernel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
