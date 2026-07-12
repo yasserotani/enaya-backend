f07f2ed5bc97abf383df30a71acc24b86f120a24
yasserotani <yasserotane@gmail.com>
2026-07-13 01:21:58 +0300

    fixing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-        apiPrefix: '',
+        apiPrefix: env('VERCEL') ? '' : 'api',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
