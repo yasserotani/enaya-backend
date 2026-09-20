a346cdbda4f6246bee7bd25afde692b77bfb9d67
yasserotani <Yasser.Aotane@Beeorder.com>
2026-09-20 13:41:52 +0300

    implement login with google 2

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Spatie\Permission\Models\Role;
 
